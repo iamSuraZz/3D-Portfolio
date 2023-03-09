9cbaf64749d03002b068fe677c1bb7423f793075
SuraZz007 <surajdev9886@gmail.com>
2023-03-09 16:22:25 +0530

    add heading a paragraph for self

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,12 +14,16 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1>
-            {" "}
+          <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Suraj</span>
           </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop <br className="sm:block hidden" /> full stack applications
+            in react
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
