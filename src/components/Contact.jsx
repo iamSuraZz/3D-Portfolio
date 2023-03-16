bd084310d15abf49597d4b80f940c68c364851e2
SuraZz007 <surajdev9886@gmail.com>
2023-03-16 13:39:46 +0530

    make bg-stars and contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,48 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_sqokwea",
+        "template_mnkgxkr",
+        {
+          from_name: form.name,
+          to_name: "Suraj Poddar",
+          from_email: form.email,
+          to_email: "surajdev9886@gmail.com",
+          message: form.message,
+        },
+        "rrpZu8gV4zKuZao1H"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
